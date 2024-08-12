26632c00cfc038c1721365ebf0e564d15a44ee16
sgmselli <sellings.matthew@gmail.com>
2024-08-12 11:07:12 +0100

    welcome message based on time of day

--- src/components/menu components/menu_title.jsx
+++ src/components/menu components/menu_title.jsx
@@ -7,10 +7,23 @@ const Menu_Title = () => {
 
     const { colorMode } = useColorMode();
 
+    const getGreeting = () => {
+        const now = new Date();
+        const hour = now.getHours(); // Get the current hour (0-23)
+    
+        if (hour >= 4 && hour < 12) {
+            return "Good Morning";
+        } else if (hour >= 12 && hour < 18) {
+            return "Good Afternoon";
+        } else {
+            return "Good Evening";
+        }
+    }
+
     return (
         <HStack gap='16px'>
             <HStack  color={colorMode === 'light' ? 'gray.600' : 'gray.300'} fontSize={{base: '20px', md:'28px'}}>
-                <Text className="rubik-bold" whiteSpace='nowrap'>Good morning,</Text>
+                <Text className="rubik-bold" whiteSpace='nowrap'>{getGreeting()},</Text>
                 <Text className="rubik-extrabold">Matthew! </Text>
             </HStack>
             <Logo pixels={28} />
